cfb3d53b2d859967809a1e8fa78d259d91d01f00
thelittlewozniak <nathan.pire@gmail.com>
2019-01-12 23:39:48 +0100

    creation the activity

---
+++ app/src/main/java/com/thelittlewozniak/theitguys/ConversationActivity.java
@@ -0,0 +1,77 @@
+package com.thelittlewozniak.theitguys;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thelittlewozniak.theitguys.pojo.Conversation;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by natha on 1/12/2019.
+ */
+
+public class ConversationActivity extends AppCompatActivity {
+    private Activity activity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activity = this;
+        setContentView(R.layout.activity_conversationlist);
+        TableLayout tableLayout = findViewById(R.id.listConversation);
+        List<Conversation> conversations = null;
+        try {
+            conversations = new ObjectMapper().readValue(getIntent().getExtras().getString("conversation"), new TypeReference<List<Conversation>>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (conversations != null) {
+            for (int i = 0; i < conversations.size(); i++) {
+                TableRow tr = new TableRow(this);
+                TextView tv = new TextView(this);
+                tv.setText(conversations.get(i).getSujet());
+                tv.setTypeface(Typeface.DEFAULT, Typeface.BOLD_ITALIC);
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 20);
+                tv.setPadding(40, 40, 40, 40);
+                if (i % 2 != 0) {
+                    tr.setBackgroundColor(ContextCompat.getColor(this, R.color.colorAccent));
+                    tv.setTextColor(ContextCompat.getColor(this, R.color.colorPrimary));
+                } else {
+                    tr.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimary));
+                    tv.setTextColor(ContextCompat.getColor(this, R.color.colorAccent));
+                }
+                tr.addView(tv);
+                tableLayout.addView(tr);
+            }
+        }
+        Button addConversation = findViewById(R.id.buttonAddConversation);
+        addConversation.setOnClickListener(new View.OnClickListener() {
+                                               public void onClick(View v) {
+                                                   activity.startActivity(new Intent(activity, AddConversationActivity.class));
+                                                   activity.finish();
+                                               }
+                                           }
+        );
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        new ConversationListAsync(activity).execute();
+    }
+}
