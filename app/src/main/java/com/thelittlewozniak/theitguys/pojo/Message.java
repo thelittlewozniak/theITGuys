0bbc9357e924bbd33ee1efcbff4dec4ed95588aa
thelittlewozniak <nathan.pire@gmail.com>
2019-01-12 14:00:55 +0100

    creating Message pojo

---
+++ app/src/main/java/com/thelittlewozniak/theitguys/pojo/Message.java
@@ -0,0 +1,41 @@
+package com.thelittlewozniak.theitguys.pojo;
+
+import java.util.Date;
+
+/**
+ * Created by natha on 1/12/2019.
+ */
+
+public class Message {
+
+    private  int id;
+
+    private String text;
+
+    private Date date;
+
+    public Conversation conversation;
+
+    public Utilisateur utilisateur;
+
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getText() {return text;}
+
+    public void setText(String text) {this.text = text;}
+
+    public Date getDate() {return date;}
+
+    public void setDate(Date date) {this.date = date;}
+
+    public Conversation getConversation() {return conversation;}
+
+    public void setConversation(Conversation conversation) {this.conversation = conversation;}
+
+    public Utilisateur getUtilisateur() {return utilisateur;}
+
+    public void setUtilisateur(Utilisateur utilisateur) {this.utilisateur = utilisateur;}
+}
