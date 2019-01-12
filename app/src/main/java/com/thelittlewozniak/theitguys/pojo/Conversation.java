edb811ab95f030ca38f4b767812b80943bdc557f
thelittlewozniak <nathan.pire@gmail.com>
2019-01-12 13:53:54 +0100

    creating Conversation pojo

---
+++ app/src/main/java/com/thelittlewozniak/theitguys/pojo/Conversation.java
@@ -0,0 +1,19 @@
+package com.thelittlewozniak.theitguys.pojo;
+
+/**
+ * Created by natha on 1/12/2019.
+ */
+
+public class Conversation {
+    private int id;
+    private String sujet;
+
+
+    public int getId(){return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getSujet() {return sujet;}
+
+    public void setSujet(String sujet) {this.sujet = sujet;}
+}
