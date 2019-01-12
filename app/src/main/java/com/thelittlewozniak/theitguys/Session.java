ad184660ad5f8db08410a1acbb94b31cc9681d25
thelittlewozniak <nathan.pire@gmail.com>
2019-01-12 23:05:38 +0100

    Making the class singleton

--- app/src/main/java/com/thelittlewozniak/theitguys/Session.java
+++ app/src/main/java/com/thelittlewozniak/theitguys/Session.java
@@ -1,8 +1,30 @@
 package com.thelittlewozniak.theitguys;
 
+import com.thelittlewozniak.theitguys.pojo.Utilisateur;
+
 /**
  * Created by natha on 1/12/2019.
  */
 
 public class Session {
+    private static Session instance;
+    
+    private Utilisateur user;
+
+
+    public Utilisateur getUser(){return user;}
+
+    public void setUser(Utilisateur user){this.user=user;}
+
+    public static Session getInstance(){
+        if(instance == null){
+            instance = new Session();
+            instance.user=null;
+        }
+        return instance;
+    }
+
+
+    private Session(){}
+
 }
