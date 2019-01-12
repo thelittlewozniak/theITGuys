cf380cd5f9650ef704dcbc4f54ff8c4ff22744cc
thelittlewozniak <nathan.pire@gmail.com>
2019-01-12 18:23:58 +0100

    creating the class

---
+++ app/src/main/java/com/thelittlewozniak/theitguys/ConversationListAsync.java
@@ -0,0 +1,18 @@
+package com.thelittlewozniak.theitguys;
+
+import android.os.AsyncTask;
+
+import com.thelittlewozniak.theitguys.pojo.Conversation;
+import com.thelittlewozniak.theitguys.pojo.Utilisateur;
+
+/**
+ * Created by natha on 1/12/2019.
+ */
+
+public class ConversationListAsync extends AsyncTask<String,String,Conversation> {
+    @Override
+    protected Conversation doInBackground(String... strings) {
+        
+        return null;
+    }
+}
