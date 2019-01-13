40fb220663ae6865fc54145596251ca5973aca11
thelittlewozniak <nathan.pire@gmail.com>
2019-01-13 13:13:58 +0100

    creating th async method

--- app/src/main/java/com/thelittlewozniak/theitguys/ProfileActivity.java
+++ app/src/main/java/com/thelittlewozniak/theitguys/ProfileActivity.java
@@ -3,6 +3,13 @@ package com.thelittlewozniak.theitguys;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thelittlewozniak.theitguys.pojo.Utilisateur;
+
+import java.io.IOException;
 
 /**
  * Created by natha on 1/13/2019.
@@ -15,5 +22,17 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        Utilisateur utilisateur=null;
+        try {
+            utilisateur= new ObjectMapper().readValue(getIntent().getExtras().getString("utilisateur"), new TypeReference<Utilisateur>() {});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(utilisateur!=null){
+            TextView pseudo=findViewById(R.id.pseudoText);
+            pseudo.setText(utilisateur.getPseudo());
+            TextView ville=findViewById(R.id.cityText);
+            ville.setText(utilisateur.getVille());
+        }
     }
 }
---
+++ app/src/main/java/com/thelittlewozniak/theitguys/ProfileAsync.java
@@ -0,0 +1,8 @@
+package com.thelittlewozniak.theitguys;
+
+/**
+ * Created by natha on 1/13/2019.
+ */
+
+public class ProfileAsync {
+}
