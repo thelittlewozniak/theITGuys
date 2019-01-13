fff2b40d7cc08c24972299f2360b7d261a8bd127
thelittlewozniak <nathan.pire@gmail.com>
2019-01-13 15:26:11 +0100

    making the activity

---
+++ app/src/main/java/com/thelittlewozniak/theitguys/RegistrationActivity.java
@@ -0,0 +1,10 @@
+package com.thelittlewozniak.theitguys;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by natha on 1/13/2019.
+ */
+
+public class RegistrationActivity extends AppCompatActivity {
+}
