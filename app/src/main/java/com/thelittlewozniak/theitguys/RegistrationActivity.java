df4495bc16d5a58bdfe9c02fd5bfcbf3112152a8
thelittlewozniak <nathan.pire@gmail.com>
2019-01-13 15:47:50 +0100

    little edits

--- app/src/main/java/com/thelittlewozniak/theitguys/RegistrationActivity.java
+++ app/src/main/java/com/thelittlewozniak/theitguys/RegistrationActivity.java
@@ -1,17 +1,29 @@
 package com.thelittlewozniak.theitguys;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by natha on 1/13/2019.
  */
 
 public class RegistrationActivity extends AppCompatActivity {
+    private Activity activity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
+        Button buttonBack=findViewById(R.id.backbuttonRegister);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activity.startActivity(new Intent(activity,MainActivity.class));
+                activity.finish();
+            }
+        });
     }
 }
