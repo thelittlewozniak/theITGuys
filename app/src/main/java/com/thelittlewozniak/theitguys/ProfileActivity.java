b255ab9b965e68b263794b463b88dc08830ec56a
thelittlewozniak <nathan.pire@gmail.com>
2019-01-14 20:07:36 +0100

    little edits

--- app/src/main/java/com/thelittlewozniak/theitguys/ProfileActivity.java
+++ app/src/main/java/com/thelittlewozniak/theitguys/ProfileActivity.java
@@ -7,7 +7,13 @@ import android.widget.TextView;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.thelittlewozniak.theitguys.pojo.Utilisateur;
 
 import java.io.IOException;
@@ -16,11 +22,11 @@ import java.io.IOException;
  * Created by natha on 1/13/2019.
  */
 
-public class ProfileActivity extends AppCompatActivity{
-    private Activity activity;
-    private String iduser;
+public class ProfileActivity extends AppCompatActivity implements OnMapReadyCallback{
     private Utilisateur utilisateur;
     private GoogleMap mMap;
+    private double latitude;
+    private double longitude;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,7 +41,19 @@ public class ProfileActivity extends AppCompatActivity{
             pseudo.setText(utilisateur.getPseudo());
             TextView ville=findViewById(R.id.cityTextView);
             ville.setText(utilisateur.getVille());
+            latitude=getIntent().getExtras().getDouble("latitude");
+            longitude=getIntent().getExtras().getDouble("longitude");
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
         }
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng marker = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(marker).title("Marker of the ville of the user"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(latitude,longitude)));
+    }
 }
--- app/src/main/java/com/thelittlewozniak/theitguys/ProfileAsync.java
+++ app/src/main/java/com/thelittlewozniak/theitguys/ProfileAsync.java
@@ -7,7 +7,13 @@ import android.os.AsyncTask;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.thelittlewozniak.theitguys.pojo.Utilisateur;
+
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -22,6 +28,8 @@ import java.net.URL;
 public class ProfileAsync extends AsyncTask<String, String, Utilisateur> {
     private Activity activity;
     private Intent intent;
+    private double latitude;
+    private double longitude;
     public ProfileAsync(Activity activity){this.activity=activity;}
     @Override
     protected Utilisateur doInBackground(String... strings) {
@@ -46,6 +54,48 @@ public class ProfileAsync extends AsyncTask<String, String, Utilisateur> {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        if(utilisateur!=null){
+            try{
+                URL url = new URL("https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key=AIzaSyAdLhe5aaJVX7--a7xYQMfK-nLEINWDMLY&input="+utilisateur.getVille()+"&inputtype=textquery&fields=geometry");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.setRequestProperty("Content-Type", "text/plain");
+                InputStream is = urlConnection.getInputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = br.readLine()) != null) {
+                    result += line;
+                }
+                br.close();
+                is.close();
+                urlConnection.disconnect();
+                JsonParser parser=new JsonParser();
+                JsonElement jsonTree = parser.parse(result);
+                if(jsonTree.isJsonObject()){
+                    JsonObject jsonObject = jsonTree.getAsJsonObject();
+
+                    JsonElement candidates = jsonObject.get("candidates");
+                    if(candidates.isJsonArray()){
+                        JsonArray jsonArray=candidates.getAsJsonArray();
+                        for (int i = 0; i < jsonArray.size(); i++) {
+                            if(jsonArray.get(i).isJsonObject()){
+                                JsonObject canditate=jsonArray.get(i).getAsJsonObject();
+                                JsonElement geometry=canditate.get("geometry");
+                                JsonObject data=geometry.getAsJsonObject();
+                                JsonElement location=data.get("location");
+                                JsonObject latlong=location.getAsJsonObject();
+                                latitude=latlong.get("lat").getAsDouble();
+                                longitude=latlong.get("lng").getAsDouble();
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+
+        }
         return utilisateur;
     }
 
@@ -60,6 +110,8 @@ public class ProfileAsync extends AsyncTask<String, String, Utilisateur> {
             } catch (JsonProcessingException e) {
                 e.printStackTrace();
             }
+            intent.putExtra("latitude",latitude);
+            intent.putExtra("longitude",longitude);
             activity.startActivityForResult(intent, 3);
         }
     }
